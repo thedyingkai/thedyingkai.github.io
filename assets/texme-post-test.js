6306f3e449b722d779e28da61ae0c919dd901588
thedyingkai <110606565+thedyingkai@users.noreply.github.com>
2026-06-23 23:16:19 +0800

    test: slice ok

--- assets/texme-post-test.js
+++ assets/texme-post-test.js
@@ -1,4 +1,7 @@
 const R='thedyingkai/thedyingkai.github.io';
 const B='main';
-function f(x){return x+1;}
-console.log(R,B,f(1));
+function f(text){
+  if(text.slice(0,4)==='---\n')return text.indexOf('\n---\n',4);
+  return -1;
+}
+console.log(R,B,f('x'));
