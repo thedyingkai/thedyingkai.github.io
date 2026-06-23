0cf57677b32e3bdd063486c809ef1cb4cf4ae24d
thedyingkai <110606565+thedyingkai@users.noreply.github.com>
2026-06-23 23:17:50 +0800

    test: inner html

--- assets/texme-post-test.js
+++ assets/texme-post-test.js
@@ -1,11 +1,7 @@
 const R='thedyingkai/thedyingkai.github.io';
-const B='main';
-function f(file,text){
-  let b=text;
-  if(text.slice(0,4)==='---\n'){
-    let end=text.indexOf('\n---\n',4);
-    if(end>=0)b=text.slice(end+5);
-  }
-  return b;
+function f(){
+  let x=document.createElement('div');
+  x.innerHTML='<p>x</p>';
+  return x;
 }
-console.log(R,B,f('x','y'));
+console.log(R,f());
