f9ebedf10692516daf980976c351b7454e34ff93
thedyingkai <110606565+thedyingkai@users.noreply.github.com>
2026-06-23 18:10:37 +0800

    chore: add push helper

---
+++ push.ps1
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "feat: init personal blog"
+git branch -M main
+git remote add origin https://github.com/thedyingkai/thedyingkai.github.io.git
+git push -u origin main
